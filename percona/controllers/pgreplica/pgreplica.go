fd18155da6577dd9be3de3798b78931cd3c2704d
Max Dudin <dudin.mv@gmail.com>
2021-07-26 18:09:32 +0300

    K8SPG-80 Update replica service logic

--- percona/controllers/pgreplica/pgreplica.go
+++ percona/controllers/pgreplica/pgreplica.go
@@ -2,7 +2,6 @@ package pgreplica
 
 import (
 	"context"
-	"fmt"
 	"reflect"
 	"strconv"
 	"strings"
@@ -12,14 +11,13 @@ import (
 	"github.com/percona/percona-postgresql-operator/internal/kubeapi"
 	"github.com/percona/percona-postgresql-operator/internal/operator/pvc"
 	"github.com/percona/percona-postgresql-operator/percona/controllers/pmm"
+	"github.com/percona/percona-postgresql-operator/percona/controllers/service"
 	crv1 "github.com/percona/percona-postgresql-operator/pkg/apis/crunchydata.com/v1"
 
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 func Create(clientset kubeapi.Interface, cluster *crv1.PerconaPGCluster) error {
@@ -27,7 +25,7 @@ func Create(clientset kubeapi.Interface, cluster *crv1.PerconaPGCluster) error {
 	if cluster.Spec.PGReplicas.HotStandby.Size == 0 {
 		return nil
 	}
-	err := createOrUpdateReplicaService(clientset, cluster)
+	err := service.CreateOrUpdate(clientset, cluster, service.PGReplicaServiceType)
 	if err != nil {
 		return errors.Wrap(err, "handle replica service")
 	}
@@ -72,7 +70,7 @@ func Update(clientset kubeapi.Interface, newCluster, oldCluster *crv1.PerconaPGC
 	}
 
 	if newReplicaCount > 0 {
-		err = createOrUpdateReplicaService(clientset, newCluster)
+		err = service.CreateOrUpdate(clientset, newCluster, service.PGReplicaServiceType)
 		if err != nil {
 			return errors.Wrap(err, "handle replica service")
 		}
@@ -85,7 +83,7 @@ func Update(clientset kubeapi.Interface, newCluster, oldCluster *crv1.PerconaPGC
 				return errors.Wrapf(err, "delete replica %s", getReplicaName(oldCluster, i))
 			}
 		}
-		err = clientset.CoreV1().Services(newCluster.Namespace).Delete(ctx, getReplicaServiceName(newCluster.Name), metav1.DeleteOptions{})
+		err = service.Delete(clientset, newCluster.Namespace, service.GetReplicaServiceName(newCluster.Name))
 		if err != nil {
 			return errors.Wrapf(err, "delete replicas service")
 		}
@@ -229,89 +227,6 @@ func getNewReplicaObject(cluster *crv1.PerconaPGCluster, replica *crv1.Pgreplica
 	}
 
 	return replica
-}
-
-func createOrUpdateReplicaService(clientset kubeapi.Interface, cluster *crv1.PerconaPGCluster) error {
-	ctx := context.TODO()
-	service, err := getReplicaServiceObject(cluster)
-	if err != nil {
-		return errors.Wrap(err, "get replica service object")
-	}
-
-	oldSvc, err := clientset.CoreV1().Services(cluster.Namespace).Get(ctx, getReplicaServiceName(cluster.Name), metav1.GetOptions{})
-	if err != nil {
-		_, err = clientset.CoreV1().Services(cluster.Namespace).Create(ctx, service, metav1.CreateOptions{})
-		if err != nil {
-			return errors.Wrap(err, "create replica service")
-		}
-		return nil
-	}
-	if reflect.DeepEqual(service.Spec, oldSvc.Spec) {
-		return nil
-	}
-	service.ResourceVersion = oldSvc.ResourceVersion
-	service.Spec.ClusterIP = oldSvc.Spec.ClusterIP
-	_, err = clientset.CoreV1().Services(cluster.Namespace).Update(ctx, service, metav1.UpdateOptions{})
-	if err != nil {
-		return errors.Wrap(err, "update replica service")
-	}
-
-	return nil
-}
-
-func getReplicaServiceObject(cluster *crv1.PerconaPGCluster) (*corev1.Service, error) {
-	replicaName := getReplicaServiceName(cluster.Name)
-	labels := map[string]string{
-		"name":       replicaName,
-		"pg-cluster": cluster.Name,
-	}
-
-	for k, v := range cluster.Spec.PGReplicas.HotStandby.Expose.Labels {
-		labels[k] = v
-	}
-
-	port, err := strconv.Atoi(cluster.Spec.Port)
-	if err != nil {
-		return &corev1.Service{}, errors.Wrap(err, "parse port")
-	}
-
-	return &corev1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:        replicaName,
-			Namespace:   cluster.Namespace,
-			Labels:      labels,
-			Annotations: cluster.Spec.PGReplicas.HotStandby.Expose.Annotations,
-		},
-		Spec: corev1.ServiceSpec{
-			Type: cluster.Spec.PGReplicas.HotStandby.Expose.ServiceType,
-			Ports: []corev1.ServicePort{
-				{
-					Name:       "sshd",
-					Protocol:   corev1.ProtocolTCP,
-					Port:       2022,
-					TargetPort: intstr.FromInt(2022),
-					NodePort:   0,
-				},
-				{
-					Name:       "postgres",
-					Protocol:   corev1.ProtocolTCP,
-					Port:       int32(port),
-					TargetPort: intstr.FromInt(port),
-					NodePort:   0,
-				},
-			},
-			Selector: map[string]string{
-				"pg-cluster": cluster.Name,
-				"role":       "replica",
-			},
-			SessionAffinity:          corev1.ServiceAffinityNone,
-			LoadBalancerSourceRanges: cluster.Spec.PGReplicas.HotStandby.Expose.LoadBalancerSourceRanges,
-		},
-	}, nil
-}
-
-func getReplicaServiceName(clusterName string) string {
-	return fmt.Sprintf("%s-replica", clusterName)
 }
 
 func updateResources(cl *crv1.PerconaPGCluster, deployment *appsv1.Deployment) {
--- percona/controllers/service/service.go
+++ percona/controllers/service/service.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"reflect"
 	"strconv"
+	"strings"
 
 	"github.com/percona/percona-postgresql-operator/internal/kubeapi"
 	crv1 "github.com/percona/percona-postgresql-operator/pkg/apis/crunchydata.com/v1"
@@ -19,6 +21,7 @@ type ServiceType string
 const (
 	PGPrimaryServiceType = ServiceType("primary")
 	PGBouncerServiceType = ServiceType("bouncer")
+	PGReplicaServiceType = ServiceType("replica")
 )
 
 func CreateOrUpdate(clientset kubeapi.Interface, cluster *crv1.PerconaPGCluster, svcType ServiceType) error {
@@ -35,6 +38,12 @@ func CreateOrUpdate(clientset kubeapi.Interface, cluster *crv1.PerconaPGCluster,
 		svc, err := getPGBouncerServiceObject(cluster)
 		if err != nil {
 			return errors.Wrap(err, "get pgBouncer service object")
+		}
+		service = svc
+	case PGReplicaServiceType:
+		svc, err := getReplicaServiceObject(cluster)
+		if err != nil {
+			return errors.Wrap(err, "get replica service object")
 		}
 		service = svc
 	}
@@ -56,6 +65,15 @@ func CreateOrUpdate(clientset kubeapi.Interface, cluster *crv1.PerconaPGCluster,
 		return errors.Wrapf(err, "update service %s", svcType)
 	}
 
+	return nil
+}
+
+func Delete(clientset kubeapi.Interface, namespace, serviceName string) error {
+	ctx := context.TODO()
+	err := clientset.CoreV1().Services(namespace).Delete(ctx, serviceName, metav1.DeleteOptions{})
+	if err != nil && strings.Contains(err.Error(), "not found") {
+		return errors.Wrapf(err, "delete replicas service")
+	}
 	return nil
 }
 
@@ -153,4 +171,59 @@ func getPGBouncerServiceObject(cluster *crv1.PerconaPGCluster) (*corev1.Service,
 			LoadBalancerSourceRanges: cluster.Spec.PGBouncer.Expose.LoadBalancerSourceRanges,
 		},
 	}, nil
+}
+
+func getReplicaServiceObject(cluster *crv1.PerconaPGCluster) (*corev1.Service, error) {
+	replicaName := GetReplicaServiceName(cluster.Name)
+	labels := map[string]string{
+		"name":       replicaName,
+		"pg-cluster": cluster.Name,
+	}
+
+	for k, v := range cluster.Spec.PGReplicas.HotStandby.Expose.Labels {
+		labels[k] = v
+	}
+
+	port, err := strconv.Atoi(cluster.Spec.Port)
+	if err != nil {
+		return &corev1.Service{}, errors.Wrap(err, "parse port")
+	}
+
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        replicaName,
+			Namespace:   cluster.Namespace,
+			Labels:      labels,
+			Annotations: cluster.Spec.PGReplicas.HotStandby.Expose.Annotations,
+		},
+		Spec: corev1.ServiceSpec{
+			Type: cluster.Spec.PGReplicas.HotStandby.Expose.ServiceType,
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "sshd",
+					Protocol:   corev1.ProtocolTCP,
+					Port:       2022,
+					TargetPort: intstr.FromInt(2022),
+					NodePort:   0,
+				},
+				{
+					Name:       "postgres",
+					Protocol:   corev1.ProtocolTCP,
+					Port:       int32(port),
+					TargetPort: intstr.FromInt(port),
+					NodePort:   0,
+				},
+			},
+			Selector: map[string]string{
+				"pg-cluster": cluster.Name,
+				"role":       "replica",
+			},
+			SessionAffinity:          corev1.ServiceAffinityNone,
+			LoadBalancerSourceRanges: cluster.Spec.PGReplicas.HotStandby.Expose.LoadBalancerSourceRanges,
+		},
+	}, nil
+}
+
+func GetReplicaServiceName(clusterName string) string {
+	return fmt.Sprintf("%s-replica", clusterName)
 }
