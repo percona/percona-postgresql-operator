55c58a4d29778b9bebf573578779461bc8620b37
Max Dudin <dudin.mv@gmail.com>
2022-02-07 16:24:00 +0200

    K8SPG-112 Clean-up`

--- percona/controllers/pgc/tls.go
+++ percona/controllers/pgc/tls.go
@@ -35,50 +35,6 @@ func (c *Controller) handleTLS(cr *crv1.PerconaPGCluster) error {
 	}
 
 	return nil
-	/*
-			ca, cert, key, err := tls.Issue([]string{cluster.Name, cluster.Name + "-pgbouncer"})
-			if err != nil {
-				return errors.Wrap(err, "issue TLS")
-			}
-			ctx := context.TODO()
-			caSecretName := cluster.Name + "-ssl-ca"
-			if len(cluster.Spec.SSLCA) > 0 {
-				caSecretName = cluster.Spec.SSLCA
-			}
-			caSecret := v1.Secret{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: caSecretName,
-				},
-				Data: map[string][]byte{
-					"ca.crt": ca,
-				},
-			}
-			keyPairSecretName := cluster.Name + "-ssl-keypair"
-			if len(cluster.Spec.SSLSecretName) > 0 {
-				keyPairSecretName = cluster.Spec.SSLSecretName
-			}
-			keyPairSecret := v1.Secret{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: keyPairSecretName,
-				},
-				Data: map[string][]byte{
-					"tls.crt": cert,
-					"tls.key": key,
-				},
-				Type: v1.SecretTypeTLS,
-			}
-
-			_, err = c.Client.CoreV1().Secrets(cluster.Namespace).Create(ctx, &caSecret, metav1.CreateOptions{})
-			if err != nil && !kerrors.IsAlreadyExists(err) {
-				return errors.Wrapf(err, "create secret %s", caSecret.Name)
-			}
-			_, err = c.Client.CoreV1().Secrets(cluster.Namespace).Create(ctx, &keyPairSecret, metav1.CreateOptions{})
-			if err != nil && !kerrors.IsAlreadyExists(err) {
-				return errors.Wrapf(err, "create secret %s", keyPairSecret.Name)
-			}
-
-		return nil
-	*/
 }
 
 func (c *Controller) createSSLManualy(cluster *crv1.PerconaPGCluster) error {
