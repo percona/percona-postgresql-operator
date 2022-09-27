a6eb01b37b773e404f8325241c2ea256df96d14d
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2022-09-27 18:25:02 +0300

    add CODEOWNERS file
        * add scan for CVE
        * add dep bot

--- internal/upgradecheck/http.go
+++ internal/upgradecheck/http.go
@@ -141,7 +141,7 @@ func checkForUpgrades(ctx context.Context, versionString string, backoff wait.Ba
 }
 
 // CheckForUpgradesScheduler invokes the check func when the operator starts
-// and then on the given period schedule. It stops when the context is cancelled.
+// and then on the given period schedule. It stops when the context is canceled.
 func CheckForUpgradesScheduler(ctx context.Context,
 	versionString, url string, crclient crclient.Client,
 	cfg *rest.Config, isOpenShift bool,
