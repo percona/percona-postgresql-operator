21384847b5e5a3304e85ccb1a01c2342c8b2b810
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2022-09-27 17:34:47 +0300

    K8SPG-257 create build script and update Dockerfile

---
+++ e2e-tests/vars.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export ROOT_REPO=${ROOT_REPO:-${PWD}}
+
+export DEPLOY_DIR="${DEPLOY_DIR:-${ROOT_REPO}/deploy}"
+export TESTS_DIR="${TESTS_DIR:-${ROOT_REPO}/e2e-tests}"
+export TESTS_CONFIG_DIR="${TESTS_CONFIG_DIR:-${TESTS_DIR}/conf}"
+export TEMP_DIR=$(mktemp -d)
+
+export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+export VERSION=${VERSION:-$(echo "${GIT_BRANCH}" | sed -e 's^/^-^g; s^[.]^-^g;' | tr '[:upper:]' '[:lower:]')}
+
+export IMAGE=${IMAGE:-"perconalab/percona-postgresql-operator:${VERSION}"}
+
+date=$(which gdate || which date)
+
+if command -v oc &> /dev/null; then
+	if oc get projects; then
+		export OPENSHIFT=4
+	fi
+fi
