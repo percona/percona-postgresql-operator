af0a012f95baddc544aee2b4790be5ff488b2e06
Andrii Dema <a.dema@jazzserve.com>
2023-04-06 12:53:52 +0300

    add unit-test

--- percona/controllers/pgcluster_test.go
+++ percona/controllers/pgcluster_test.go
@@ -5,10 +5,12 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var _ = Describe("PG Cluster", Ordered, func() {
@@ -54,3 +56,150 @@ var _ = Describe("PG Cluster", Ordered, func() {
 		})
 	})
 })
+
+var _ = Describe("PMM sidecar", Ordered, func() {
+	ctx := context.Background()
+
+	const crName = "pmm-test"
+	const ns = crName
+	crNamespacedName := types.NamespacedName{Name: crName, Namespace: ns}
+
+	namespace := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      crName,
+			Namespace: ns,
+		},
+	}
+
+	BeforeAll(func() {
+		By("Creating the Namespace to perform the tests")
+		err := k8sClient.Create(ctx, namespace)
+		Expect(err).To(Not(HaveOccurred()))
+	})
+
+	AfterAll(func() {
+		By("Deleting the Namespace to perform the tests")
+		_ = k8sClient.Delete(ctx, namespace)
+	})
+
+	cr, err := readDefaultCR(crName, ns)
+	It("should read defautl cr.yaml", func() {
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should create PerconaPGCluster with pmm enabled", func() {
+		cr.Spec.PMM.Enabled = true
+		Expect(k8sClient.Create(ctx, cr)).Should(Succeed())
+	})
+
+	Context("Reconcile controller", func() {
+		It("Controller should reconcile", func() {
+			_, err := reconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+			_, err = crunchyReconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+	sts := appsv1.StatefulSet{}
+	Context("sts should not have pmm sidecar", func() {
+		It("should get sts", func() {
+			stsList := &appsv1.StatefulSetList{}
+			labels := map[string]string{
+				"postgres-operator.crunchydata.com/data":         "postgres",
+				"postgres-operator.crunchydata.com/instance-set": "instance1",
+				"postgres-operator.crunchydata.com/cluster":      crName,
+			}
+			err = k8sClient.List(ctx, stsList, client.InNamespace(cr.Namespace), client.MatchingLabels(labels))
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(stsList.Items).NotTo(BeEmpty())
+			sts = stsList.Items[0]
+		})
+		It("should not have pmm container", func() {
+			Expect(havePMMSidecar(sts)).To(BeFalse())
+		})
+	})
+
+	Context("secret with no data", func() {
+		It("should create pmm secret with no data", func() {
+			Expect(k8sClient.Create(ctx, &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "cluster1-pmm-secret",
+					Namespace: ns,
+				},
+				Data: map[string][]byte{
+					"PMM_SERVER_KEY": {},
+				},
+			})).Should(Succeed())
+		})
+		It("Controller should reconcile", func() {
+			_, err := reconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+			_, err = crunchyReconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+		})
+		It("should get updated sts", func() {
+			Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(&sts), &sts)).Should(Succeed())
+		})
+		It("should not have pmm container", func() {
+			Expect(havePMMSidecar(sts)).To(BeFalse())
+		})
+	})
+
+	Context("secret with data", func() {
+		It("should fill pmm secret with data", func() {
+			Expect(k8sClient.Update(ctx, &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "cluster1-pmm-secret",
+					Namespace: ns,
+				},
+				Data: map[string][]byte{
+					"PMM_SERVER_KEY": []byte("some-data"),
+				},
+			})).Should(Succeed())
+		})
+		It("Controller should reconcile", func() {
+			_, err := reconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+			_, err = crunchyReconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+		})
+		It("should get updated sts", func() {
+			Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(&sts), &sts)).Should(Succeed())
+		})
+		It("should have pmm container", func() {
+			Expect(havePMMSidecar(sts)).To(BeTrue())
+		})
+	})
+
+	Context("cr with disabled pmm", func() {
+		It("should disable pmm", func() {
+			Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(cr), cr)).Should(Succeed())
+			cr.Spec.PMM.Enabled = false
+			Expect(k8sClient.Update(ctx, cr)).Should(Succeed())
+		})
+		It("Controller should reconcile", func() {
+			_, err := reconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+			_, err = crunchyReconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+		})
+		It("should get updated sts", func() {
+			Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(&sts), &sts)).Should(Succeed())
+		})
+		It("should not have pmm container", func() {
+			Expect(havePMMSidecar(sts)).To(BeFalse())
+		})
+	})
+})
+
+func havePMMSidecar(sts appsv1.StatefulSet) bool {
+	containers := sts.Spec.Template.Spec.Containers
+	for _, container := range containers {
+		if container.Name == "pmm-client" {
+			return true
+		}
+	}
+	return false
+}
--- percona/controllers/suite_test.go
+++ percona/controllers/suite_test.go
@@ -7,14 +7,19 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"go.opentelemetry.io/otel"
 	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/component-base/featuregate"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
+	"github.com/percona/percona-postgresql-operator/internal/controller/postgrescluster"
+	"github.com/percona/percona-postgresql-operator/internal/util"
 	"github.com/percona/percona-postgresql-operator/pkg/apis/pg.percona.com/v2beta1"
 	"github.com/percona/percona-postgresql-operator/pkg/apis/postgres-operator.crunchydata.com/v1beta1"
 	//+kubebuilder:scaffold:imports
@@ -44,6 +49,14 @@ var _ = BeforeSuite(func() {
 		ErrorIfCRDPathMissing: true,
 	}
 
+	features := map[featuregate.Feature]featuregate.FeatureSpec{
+		util.TablespaceVolumes: {Default: true},
+		util.InstanceSidecars:  {Default: true},
+		util.PGBouncerSidecars: {Default: true},
+	}
+
+	util.DefaultMutableFeatureGate.Add(features)
+
 	var err error
 	cfg, err = testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
@@ -72,6 +85,15 @@ func reconciler() *PGClusterReconciler {
 		Platform:    "unknown",
 		KubeVersion: "1.25",
 	})
+}
+
+func crunchyReconciler() *postgrescluster.Reconciler {
+	return &postgrescluster.Reconciler{
+		Client:   k8sClient,
+		Owner:    postgrescluster.ControllerName,
+		Recorder: new(record.FakeRecorder),
+		Tracer:   otel.Tracer("test"),
+	}
 }
 
 func readDefaultCR(name, namespace string) (*v2beta1.PerconaPGCluster, error) {
