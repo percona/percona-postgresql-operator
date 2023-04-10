58cb784cb7832eda9e81ab10429926eb6cce167e
Andrii Dema <a.dema@jazzserve.com>
2023-04-10 15:04:43 +0300

    improve unit-test

--- percona/controllers/pgcluster_test.go
+++ percona/controllers/pgcluster_test.go
@@ -102,94 +102,95 @@ var _ = Describe("PMM sidecar", Ordered, func() {
 	})
 
 	sts := appsv1.StatefulSet{}
-	Context("sts should not have pmm sidecar", func() {
-		It("should get sts", func() {
-			stsList := &appsv1.StatefulSetList{}
-			labels := map[string]string{
-				"postgres-operator.crunchydata.com/data":         "postgres",
-				"postgres-operator.crunchydata.com/instance-set": "instance1",
-				"postgres-operator.crunchydata.com/cluster":      crName,
-			}
-			err = k8sClient.List(ctx, stsList, client.InNamespace(cr.Namespace), client.MatchingLabels(labels))
-			Expect(err).NotTo(HaveOccurred())
 
-			Expect(stsList.Items).NotTo(BeEmpty())
-			sts = stsList.Items[0]
-		})
-		It("should not have pmm container", func() {
-			Expect(havePMMSidecar(sts)).To(BeFalse())
-		})
+	It("should get instance-set sts", func() {
+		stsList := &appsv1.StatefulSetList{}
+		labels := map[string]string{
+			"postgres-operator.crunchydata.com/data":         "postgres",
+			"postgres-operator.crunchydata.com/instance-set": "instance1",
+			"postgres-operator.crunchydata.com/cluster":      crName,
+		}
+		err = k8sClient.List(ctx, stsList, client.InNamespace(cr.Namespace), client.MatchingLabels(labels))
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(stsList.Items).NotTo(BeEmpty())
+		sts = stsList.Items[0]
 	})
 
-	Context("secret with no data", func() {
-		It("should create pmm secret with no data", func() {
-			Expect(k8sClient.Create(ctx, &corev1.Secret{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "cluster1-pmm-secret",
-					Namespace: ns,
-				},
-				Data: map[string][]byte{
-					"PMM_SERVER_KEY": {},
-				},
-			})).Should(Succeed())
+	Context("Instance-set statefulset", func() {
+		When("sts doesn't have pmm sidecar", func() {
+			It("should not have pmm container", func() {
+				Expect(havePMMSidecar(sts)).To(BeFalse())
+			})
 		})
-		It("Controller should reconcile", func() {
-			_, err := reconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
-			Expect(err).NotTo(HaveOccurred())
-			_, err = crunchyReconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
-			Expect(err).NotTo(HaveOccurred())
-		})
-		It("should get updated sts", func() {
-			Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(&sts), &sts)).Should(Succeed())
-		})
-		It("should not have pmm container", func() {
-			Expect(havePMMSidecar(sts)).To(BeFalse())
-		})
-	})
 
-	Context("secret with data", func() {
-		It("should fill pmm secret with data", func() {
-			Expect(k8sClient.Update(ctx, &corev1.Secret{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "cluster1-pmm-secret",
-					Namespace: ns,
-				},
-				Data: map[string][]byte{
-					"PMM_SERVER_KEY": []byte("some-data"),
-				},
-			})).Should(Succeed())
-		})
-		It("Controller should reconcile", func() {
-			_, err := reconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
-			Expect(err).NotTo(HaveOccurred())
-			_, err = crunchyReconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
-			Expect(err).NotTo(HaveOccurred())
-		})
-		It("should get updated sts", func() {
-			Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(&sts), &sts)).Should(Succeed())
-		})
-		It("should have pmm container", func() {
-			Expect(havePMMSidecar(sts)).To(BeTrue())
-		})
-	})
+		When("pmm secret has no data", func() {
+			BeforeAll(func() {
+				Expect(k8sClient.Create(ctx, &corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "cluster1-pmm-secret",
+						Namespace: ns,
+					},
+					Data: map[string][]byte{
+						"PMM_SERVER_KEY": {},
+					},
+				})).Should(Succeed())
 
-	Context("cr with disabled pmm", func() {
-		It("should disable pmm", func() {
-			Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(cr), cr)).Should(Succeed())
-			cr.Spec.PMM.Enabled = false
-			Expect(k8sClient.Update(ctx, cr)).Should(Succeed())
+				_, err := reconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
+				Expect(err).NotTo(HaveOccurred())
+				_, err = crunchyReconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("should not have pmm container", func() {
+				Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(&sts), &sts)).Should(Succeed())
+
+				Expect(havePMMSidecar(sts)).To(BeFalse())
+			})
 		})
-		It("Controller should reconcile", func() {
-			_, err := reconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
-			Expect(err).NotTo(HaveOccurred())
-			_, err = crunchyReconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
-			Expect(err).NotTo(HaveOccurred())
+
+		When("pmm secret has data", func() {
+			BeforeAll(func() {
+				Expect(k8sClient.Update(ctx, &corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "cluster1-pmm-secret",
+						Namespace: ns,
+					},
+					Data: map[string][]byte{
+						"PMM_SERVER_KEY": []byte("some-data"),
+					},
+				})).Should(Succeed())
+
+				_, err := reconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
+				Expect(err).NotTo(HaveOccurred())
+				_, err = crunchyReconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("should have pmm container", func() {
+				Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(&sts), &sts)).Should(Succeed())
+
+				Expect(havePMMSidecar(sts)).To(BeTrue())
+			})
 		})
-		It("should get updated sts", func() {
-			Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(&sts), &sts)).Should(Succeed())
-		})
-		It("should not have pmm container", func() {
-			Expect(havePMMSidecar(sts)).To(BeFalse())
+
+		When("cr has disabled pmm", func() {
+			BeforeAll(func() {
+				Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(cr), cr)).Should(Succeed())
+				cr.Spec.PMM.Enabled = false
+				Expect(k8sClient.Update(ctx, cr)).Should(Succeed())
+
+				_, err := reconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
+				Expect(err).NotTo(HaveOccurred())
+				_, err = crunchyReconciler().Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("should not have pmm container", func() {
+				Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(&sts), &sts)).Should(Succeed())
+
+				Expect(havePMMSidecar(sts)).To(BeFalse())
+			})
 		})
 	})
 })
