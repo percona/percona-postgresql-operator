0e9f78c414141d15c01e62e4d8ae8e6ba473cbd8
Ege Güneş <ege.gunes@percona.com>
2023-04-18 13:47:49 +0300

    Add script to build bundle images

---
+++ installers/olm/build-image.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BUNDLE_REPO="${BUNDLE_REPO:-percona/percona-postgresql-operator}"
+
+build_image() {
+	local container="$1" directory="$2" distro="$3" version="$4"
+	directory=$(cd "${directory}" && pwd)
+
+    local tag="${version}-${distro}-bundle"
+    local image="${BUNDLE_REPO}:${tag}"
+
+    pushd ${directory}
+
+    "${container}" build -t "${image}" .
+    "${container}" push "${image}"
+
+    popd
+}
+
+build_image "$@"
