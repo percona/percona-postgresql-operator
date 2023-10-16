f512f339a8547f55d607334ffdede17b260478ed
Inel Pandzic <inel.pandzic@percona.com>
2023-10-16 15:01:19 +0200

    K8SPG-372: Align cluster-wide support with other operators (#529)
    * Log
    
    * log
    
    * Add suport to watch multiple namespaces.
    
    * Add WATCH_NAMESPACE env var and generate yaml files.
    
    * Cleanup
    
    * Cleanup
    
    * Log
    
    * Fix creating manager cache.
    
    * Update percona/runtime/runtime.go
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Support PGO_TARGET_NAMESPACE for backward compaibility.
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ percona/runtime/runtime.go
@@ -0,0 +1,51 @@
+package runtime
+
+import (
+	"strings"
+	"time"
+
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	r "github.com/percona/percona-postgresql-operator/internal/controller/runtime"
+)
+
+// default refresh interval in minutes
+var refreshInterval = 60 * time.Minute
+
+// CreateRuntimeManager does the same thing as `internal/controller/runtime.CreateRuntimeManager`,
+// excet it configures the manager to watch multiple namespaces.
+func CreateRuntimeManager(namespaces string, config *rest.Config,
+	disableMetrics bool) (manager.Manager, error) {
+
+	pgoScheme, err := r.CreatePostgresOperatorScheme()
+	if err != nil {
+		return nil, err
+	}
+
+	options := manager.Options{
+		SyncPeriod: &refreshInterval,
+		Scheme:     pgoScheme,
+	}
+
+	nn := strings.Split(namespaces, ",")
+	if len(nn) > 0 && nn[0] != "" {
+		options.NewCache = cache.MultiNamespacedCacheBuilder(nn)
+	} else {
+		options.Namespace = ""
+	}
+
+	if disableMetrics {
+		options.HealthProbeBindAddress = "0"
+		options.MetricsBindAddress = "0"
+	}
+
+	// create controller runtime manager
+	mgr, err := manager.New(config, options)
+	if err != nil {
+		return nil, err
+	}
+
+	return mgr, nil
+}
