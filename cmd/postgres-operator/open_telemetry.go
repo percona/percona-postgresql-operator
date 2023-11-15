25041d1d357e5f983c0440ada21b6723f0283f6e
Inel Pandzic <inel.pandzic@percona.com>
2023-11-15 10:39:04 +0100

    Add open_telemetry_oltp.

--- cmd/postgres-operator/open_telemetry.go
+++ cmd/postgres-operator/open_telemetry.go
@@ -15,81 +15,81 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import (
-	"context"
-	"fmt"
-	"io"
-	"net/http"
-	"os"
+// import (
+// 	"context"
+// 	"fmt"
+// 	"io"
+// 	"net/http"
+// 	"os"
 
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/stdout"
-	"go.opentelemetry.io/otel/exporters/trace/jaeger"
-)
+// 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+// 	"go.opentelemetry.io/otel"
+// 	"go.opentelemetry.io/otel/exporters/stdout"
+// 	"go.opentelemetry.io/otel/exporters/trace/jaeger"
+// )
 
-func initOpenTelemetry() (func(), error) {
-	// At the time of this writing, the SDK (go.opentelemetry.io/otel@v0.13.0)
-	// does not automatically initialize any trace or metric exporter. An upcoming
-	// specification details environment variables that should facilitate this in
-	// the future.
-	//
-	// - https://github.com/open-telemetry/opentelemetry-specification/blob/f5519f2b/specification/sdk-environment-variables.md
+// func initOpenTelemetry() (func(), error) {
+// 	// At the time of this writing, the SDK (go.opentelemetry.io/otel@v0.13.0)
+// 	// does not automatically initialize any trace or metric exporter. An upcoming
+// 	// specification details environment variables that should facilitate this in
+// 	// the future.
+// 	//
+// 	// - https://github.com/open-telemetry/opentelemetry-specification/blob/f5519f2b/specification/sdk-environment-variables.md
 
-	switch os.Getenv("OTEL_EXPORTER") {
-	case "jaeger":
-		exp, err := jaeger.NewExportPipeline(jaeger.WithCollectorEndpoint())
-		if err != nil {
-			return nil, err
-		}
-		flush := func() {
-			err := exp.ForceFlush(context.TODO())
-			if err != nil {
-				fmt.Println(err)
-			}
-		}
-		return flush, nil
+// 	switch os.Getenv("OTEL_EXPORTER") {
+// 	case "jaeger":
+// 		exp, err := jaeger.NewExportPipeline(jaeger.WithCollectorEndpoint())
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		flush := func() {
+// 			err := exp.ForceFlush(context.TODO())
+// 			if err != nil {
+// 				fmt.Println(err)
+// 			}
+// 		}
+// 		return flush, nil
 
-	case "json":
-		var closer io.Closer
-		filename := os.Getenv("OTEL_JSON_FILE")
-		options := []stdout.Option{stdout.WithoutMetricExport()}
+// 	case "json":
+// 		var closer io.Closer
+// 		filename := os.Getenv("OTEL_JSON_FILE")
+// 		options := []stdout.Option{stdout.WithoutMetricExport()}
 
-		if filename != "" {
-			file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
-			if err != nil {
-				return nil, fmt.Errorf("unable to open exporter file: %w", err)
-			}
-			closer = file
-			options = append(options, stdout.WithWriter(file))
-		}
+// 		if filename != "" {
+// 			file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+// 			if err != nil {
+// 				return nil, fmt.Errorf("unable to open exporter file: %w", err)
+// 			}
+// 			closer = file
+// 			options = append(options, stdout.WithWriter(file))
+// 		}
 
-		provider, pusher, err := stdout.NewExportPipeline(options, nil)
-		if err != nil {
-			return nil, fmt.Errorf("unable to initialize stdout exporter: %w", err)
-		}
-		flush := func() {
-			pusher.Stop(context.TODO())
-			if closer != nil {
-				_ = closer.Close()
-			}
-		}
+// 		provider, pusher, err := stdout.NewExportPipeline(options, nil)
+// 		if err != nil {
+// 			return nil, fmt.Errorf("unable to initialize stdout exporter: %w", err)
+// 		}
+// 		flush := func() {
+// 			pusher.Stop(context.TODO())
+// 			if closer != nil {
+// 				_ = closer.Close()
+// 			}
+// 		}
 
-		otel.SetTracerProvider(provider)
-		return flush, nil
-	}
+// 		otel.SetTracerProvider(provider)
+// 		return flush, nil
+// 	}
 
-	// $OTEL_EXPORTER is unset or unknown, so no TracerProvider has been assigned.
-	// The default at this time is a single "no-op" tracer.
+// 	// $OTEL_EXPORTER is unset or unknown, so no TracerProvider has been assigned.
+// 	// The default at this time is a single "no-op" tracer.
 
-	return func() {}, nil
-}
+// 	return func() {}, nil
+// }
 
-// otelTransportWrapper creates a function that wraps the provided net/http.RoundTripper
-// with one that starts a span for each request, injects context into that request,
-// and ends the span when that request's response body is closed.
-func otelTransportWrapper(options ...otelhttp.Option) func(http.RoundTripper) http.RoundTripper {
-	return func(rt http.RoundTripper) http.RoundTripper {
-		return otelhttp.NewTransport(rt, options...)
-	}
-}
+// // otelTransportWrapper creates a function that wraps the provided net/http.RoundTripper
+// // with one that starts a span for each request, injects context into that request,
+// // and ends the span when that request's response body is closed.
+// func otelTransportWrapper(options ...otelhttp.Option) func(http.RoundTripper) http.RoundTripper {
+// 	return func(rt http.RoundTripper) http.RoundTripper {
+// 		return otelhttp.NewTransport(rt, options...)
+// 	}
+// }
--- go.mod
+++ go.mod
@@ -19,12 +19,13 @@ require (
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/pflag v1.0.5
 	github.com/xdg/stringprep v1.0.0
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.20.0
-	go.opentelemetry.io/otel v0.20.0
-	go.opentelemetry.io/otel/exporters/stdout v0.20.0
-	go.opentelemetry.io/otel/exporters/trace/jaeger v0.20.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.44.0
+	go.opentelemetry.io/otel v1.20.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.20.0
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.20.0
+	go.opentelemetry.io/otel/sdk v1.20.0
 	golang.org/x/crypto v0.15.0
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	golang.org/x/sync v0.3.0
 	gopkg.in/yaml.v2 v2.4.0
 	k8s.io/api v0.23.0
 	k8s.io/apimachinery v0.23.3
@@ -34,13 +35,23 @@ require (
 )
 
 require (
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/grpc v1.59.0 // indirect
+)
+
+require (
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
-	github.com/felixge/httpsnoop v1.0.1 // indirect
-	github.com/go-logr/logr v1.2.0 // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-openapi/analysis v0.19.14 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.5 // indirect
@@ -48,9 +59,9 @@ require (
 	github.com/go-openapi/spec v0.19.14 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
-	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/googleapis/gnostic v0.5.5 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
@@ -65,23 +76,18 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
 	go.mongodb.org/mongo-driver v1.5.1 // indirect
-	go.opentelemetry.io/contrib v0.20.0 // indirect
-	go.opentelemetry.io/otel/metric v0.20.0 // indirect
-	go.opentelemetry.io/otel/sdk v0.20.0 // indirect
-	go.opentelemetry.io/otel/sdk/export/metric v0.20.0 // indirect
-	go.opentelemetry.io/otel/sdk/metric v0.20.0 // indirect
-	go.opentelemetry.io/otel/trace v0.20.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.20.0
+	go.opentelemetry.io/otel/metric v1.20.0 // indirect
+	go.opentelemetry.io/otel/trace v1.20.0 // indirect
 	golang.org/x/net v0.18.0 // indirect
-	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
+	golang.org/x/oauth2 v0.11.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/term v0.14.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/apiextensions-apiserver v0.23.0 // indirect
