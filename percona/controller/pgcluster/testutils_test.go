7a53f31e08ced7fac68e4306fac74b7f77a160ac
Inel Pandzic <inel.pandzic@percona.com>
2024-12-12 11:38:31 +0100

    Fix tests.

--- percona/controller/pgcluster/testutils_test.go
+++ percona/controller/pgcluster/testutils_test.go
@@ -107,6 +107,8 @@ func readDefaultCR(name, namespace string) (*v2.PerconaPGCluster, error) {
 
 	cr.Name = name
 	cr.Namespace = namespace
+	t := true
+	cr.Spec.AutoCreateUserSchema = &t
 	return cr, nil
 }
 
