f05626124145a05f5323df843ddad8456b737770
Inel Pandzic <inel.pandzic@percona.com>
2024-12-17 12:51:15 +0100

    Fix panic

--- pkg/apis/pgv2.percona.com/v2/perconapgcluster_types.go
+++ pkg/apis/pgv2.percona.com/v2/perconapgcluster_types.go
@@ -258,7 +258,7 @@ func (cr *PerconaPGCluster) ToCrunchy(ctx context.Context, postgresCluster *crun
 		}
 	}
 
-	if *cr.Spec.AutoCreateUserSchema {
+	if cr.Spec.AutoCreateUserSchema != nil && *cr.Spec.AutoCreateUserSchema {
 		annotations[naming.AutoCreateUserSchemaAnnotation] = "true"
 	}
 
