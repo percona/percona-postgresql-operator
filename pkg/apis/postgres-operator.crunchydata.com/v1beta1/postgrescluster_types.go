2affefac2c8b0190af19f1a59d8578e7928c373f
Natalia Marukovich <natalia.marukovich@percona.com>
2025-02-24 19:34:45 +0300

    fix

--- pkg/apis/postgres-operator.crunchydata.com/v1beta1/postgrescluster_types.go
+++ pkg/apis/postgres-operator.crunchydata.com/v1beta1/postgrescluster_types.go
@@ -291,7 +291,6 @@ type PostgresClusterDataSource struct {
 	// for the new PostgresCluster.
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Pattern=^repo[1-4]
-	// +kubebuilder:default="repo1"
 	RepoName string `json:"repoName"`
 
 	// Command line options to include when running the pgBackRest restore command.
