c0aaf3ddea77ec46bbde93e305f861932c18065a
George Kechagias <geo.kechagias@gmail.com>
2025-06-28 21:05:39 +0300

    K8SPG-799 configure imagepullsecrets for patroni version check pod - add envtests (#1184)
    * K8SPG-799 configure imagepullsecrets for patroni version check pod - add envtests
    
    * fix imports
    
    * remove resources assertion - ideally whole pod should be extracted from here
    
    * fix comments
    
    * use right image
    
    ---------
    
    Co-authored-by: Viacheslav Sarzhan <slava.sarzhan@percona.com>

--- percona/controller/pgcluster/controller_test.go
+++ percona/controller/pgcluster/controller_test.go
@@ -21,6 +21,7 @@ import (
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -1514,6 +1515,186 @@ var _ = Describe("ServiceAccount early creation", Ordered, func() {
 		Expect(err).NotTo(HaveOccurred())
 		_, err = crunchyReconciler.Reconcile(ctx, ctrl.Request{NamespacedName: crNamespacedName})
 		Expect(err).NotTo(HaveOccurred())
+	})
+})
+
+var _ = Describe("patroni version check", Ordered, func() {
+	ctx := context.Background()
+
+	const crName = "patroni-version-test"
+	const ns = crName
+	crNamespacedName := types.NamespacedName{Name: crName, Namespace: ns}
+
+	namespace := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      crName,
+			Namespace: ns,
+		},
+	}
+
+	BeforeAll(func() {
+		By("Creating the Namespace to perform the tests")
+		err := k8sClient.Create(ctx, namespace)
+		Expect(err).To(Not(HaveOccurred()))
+	})
+
+	AfterAll(func() {
+		By("Deleting the Namespace to perform the tests")
+		_ = k8sClient.Delete(ctx, namespace)
+	})
+
+	Context("With custom patroni version annotation", func() {
+		cr, err := readDefaultCR(crName, ns)
+		It("should read default cr.yaml", func() {
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should create PerconaPGCluster with custom patroni version", func() {
+			if cr.Annotations == nil {
+				cr.Annotations = make(map[string]string)
+			}
+			cr.Annotations[pNaming.AnnotationCustomPatroniVersion] = "3.2.1"
+
+			status := cr.Status
+			Expect(k8sClient.Create(ctx, cr)).Should(Succeed())
+			cr.Status = status
+			Expect(k8sClient.Status().Update(ctx, cr)).Should(Succeed())
+		})
+
+		It("should successfully reconcile patroni version check", func() {
+			reconcilerInstance := reconciler(cr)
+			err := reconcilerInstance.reconcilePatroniVersionCheck(ctx, cr)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should copy custom patroni version to status", func() {
+			updatedCR := &v2.PerconaPGCluster{}
+			Expect(k8sClient.Get(ctx, crNamespacedName, updatedCR)).Should(Succeed())
+
+			Expect(updatedCR.Status.PatroniVersion).To(Equal("3.2.1"))
+		})
+	})
+
+	Context("Without custom patroni version annotation", func() {
+		const crName2 = "patroni-version-test-2"
+		const ns2 = crName2
+		crNamespacedName2 := types.NamespacedName{Name: crName2, Namespace: ns2}
+
+		namespace2 := &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      crName2,
+				Namespace: ns2,
+			},
+		}
+
+		BeforeAll(func() {
+			By("Creating the second namespace")
+			err := k8sClient.Create(ctx, namespace2)
+			Expect(err).To(Not(HaveOccurred()))
+		})
+
+		AfterAll(func() {
+			By("Deleting the second namespace")
+			_ = k8sClient.Delete(ctx, namespace2)
+		})
+
+		cr2, err := readDefaultCR(crName2, ns2)
+		It("should read default cr.yaml", func() {
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should create PerconaPGCluster without custom patroni version annotation", func() {
+			if cr2.Annotations == nil {
+				cr2.Annotations = make(map[string]string)
+			}
+			delete(cr2.Annotations, pNaming.AnnotationCustomPatroniVersion)
+
+			uid := int64(1001)
+			cr2.Spec.InstanceSets[0].SecurityContext = &corev1.PodSecurityContext{
+				RunAsUser: &uid,
+			}
+			cr2.Spec.ImagePullSecrets = []corev1.LocalObjectReference{
+				{Name: "test-pull-secret"},
+			}
+
+			cr2.Status.PatroniVersion = "3.1.0"
+			cr2.Status.Postgres.ImageID = "some-image-id"
+
+			status := cr2.Status
+			Expect(k8sClient.Create(ctx, cr2)).Should(Succeed())
+			cr2.Status = status
+			Expect(k8sClient.Status().Update(ctx, cr2)).Should(Succeed())
+
+			pod := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      cr2.Name + "-instance-pod",
+					Namespace: cr2.Namespace,
+					Labels: map[string]string{
+						"postgres-operator.crunchydata.com/cluster":  cr2.Name,
+						"postgres-operator.crunchydata.com/instance": "instance",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "database",
+							Image: "postgres:16",
+						},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, pod)).Should(Succeed())
+
+			pod.Status = corev1.PodStatus{
+				Phase: corev1.PodRunning,
+				ContainerStatuses: []corev1.ContainerStatus{
+					{
+						Name:    "database",
+						ImageID: "postgres:16",
+					},
+				},
+			}
+			Expect(k8sClient.Status().Update(ctx, pod)).Should(Succeed())
+		})
+
+		It("should create patroni version check pod and return errPatroniVersionCheckWait", func() {
+			reconcilerInstance := reconciler(cr2)
+			err := reconcilerInstance.reconcilePatroniVersionCheck(ctx, cr2)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("waiting for pod to initialize"))
+		})
+
+		It("should have created patroni version check pod with correct configuration", func() {
+			podName := cr2.Name + "-patroni-version-check"
+			pod := &corev1.Pod{}
+			err = k8sClient.Get(ctx, types.NamespacedName{Name: podName, Namespace: cr2.Namespace}, pod)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(pod.Spec.Containers).To(HaveLen(1))
+			Expect(pod.Spec.Containers[0].Name).To(Equal(pNaming.ContainerPatroniVersionCheck))
+			Expect(pod.Spec.Containers[0].Image).To(Equal(cr2.Spec.Image))
+			Expect(pod.Spec.Containers[0].Command).To(Equal([]string{"bash"}))
+			Expect(pod.Spec.Containers[0].Args).To(Equal([]string{"-c", "sleep 60"}))
+
+			uid := int64(1001)
+			expectedSecurityContext := &corev1.PodSecurityContext{
+				RunAsUser: &uid,
+			}
+			expectedImagePullSecrets := []corev1.LocalObjectReference{
+				{Name: "test-pull-secret"},
+			}
+
+			Expect(pod.Spec.SecurityContext).To(Equal(expectedSecurityContext))
+			Expect(pod.Spec.TerminationGracePeriodSeconds).To(Equal(ptr.To(int64(5))))
+			Expect(pod.Spec.ImagePullSecrets).To(Equal(expectedImagePullSecrets))
+		})
+
+		It("should preserve existing patroni version in annotation", func() {
+			updatedCR := &v2.PerconaPGCluster{}
+			Expect(k8sClient.Get(ctx, crNamespacedName2, updatedCR)).Should(Succeed())
+
+			Expect(updatedCR.Status.PatroniVersion).To(Equal("3.1.0"))
+		})
 	})
 })
 
