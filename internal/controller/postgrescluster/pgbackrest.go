0caf350049e0d52156ff68c1a7203c06e3ed3b60
Andrii Dema <a.dema@jazzserve.com>
2025-06-30 19:32:35 +0300

    add dependency

--- internal/controller/postgrescluster/pgbackrest.go
+++ internal/controller/postgrescluster/pgbackrest.go
@@ -22,6 +22,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
