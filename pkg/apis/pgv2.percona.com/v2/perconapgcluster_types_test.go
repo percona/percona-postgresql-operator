da4126145a2bb78a590e5846a82dcfd1e28832be
Jakub Jaruszewski <jjaruszewski@outlook.com>
2025-06-30 17:47:38 +0200

    K8SPG-792 Make patroni check check respect default image settings (#1167)
    * K8SPG-792 Make patroni check check respect default image settings
    
    Signed-off-by: Jakub Jaruszewski <jjaruszewski@man.poznan.pl>
    
    * K8SPG-792 Add unit tests for default image behaviour
    
    Signed-off-by: Jakub Jaruszewski <jjaruszewski@man.poznan.pl>
    
    * K8SPG-792 Add comments regarding changed upstream func
    
    Signed-off-by: Jakub Jaruszewski <jjaruszewski@man.poznan.pl>
    
    * K8SPG-792 Use table for tests
    
    Signed-off-by: Jakub Jaruszewski <jjaruszewski@man.poznan.pl>
    
    * K8SPG-792 Fix gofmt warning
    
    Signed-off-by: Jakub Jaruszewski <jjaruszewski@man.poznan.pl>
    
    ---------
    
    Signed-off-by: Jakub Jaruszewski <jjaruszewski@man.poznan.pl>
    Co-authored-by: Viacheslav Sarzhan <slava.sarzhan@percona.com>
    Co-authored-by: George Kechagias <geo.kechagias@gmail.com>

--- pkg/apis/pgv2.percona.com/v2/perconapgcluster_types_test.go
+++ pkg/apis/pgv2.percona.com/v2/perconapgcluster_types_test.go
@@ -1,6 +1,8 @@
 package v2
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"gotest.tools/v3/assert"
@@ -38,6 +40,64 @@ func TestPerconaPGCluster_BackupsEnabled(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			actual := tt.spec.Backups.IsEnabled()
 			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
+
+func TestPerconaPGCluster_PostgresImage(t *testing.T) {
+	cluster := new(PerconaPGCluster)
+	cluster.Default()
+
+	postgresVersion := 16
+	testDefaultImage := fmt.Sprintf("test_default_image:%d", postgresVersion)
+	testSpecificImage := fmt.Sprintf("test_defined_image:%d", postgresVersion)
+	testEnv := fmt.Sprintf("RELATED_IMAGE_POSTGRES_%d", postgresVersion)
+
+	cluster.Spec.PostgresVersion = postgresVersion
+
+	tests := map[string]struct {
+		expectedImage string
+		setImage      string
+		envImage      string
+	}{
+		"Spec.Image should be empty by default": {
+			expectedImage: "",
+			setImage:      "",
+			envImage:      "",
+		},
+		"Spec.Image should use env variables if present": {
+			expectedImage: testDefaultImage,
+			setImage:      "",
+			envImage:      testDefaultImage,
+		},
+		"Spec.Image should use defined variable": {
+			expectedImage: testSpecificImage,
+			setImage:      testSpecificImage,
+			envImage:      testDefaultImage,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+
+			cluster.Spec.Image = tt.setImage
+
+			if tt.envImage != "" {
+				err := os.Setenv(testEnv, tt.envImage)
+
+				if err != nil {
+					t.Fatalf("Failed to set %s env variable: %v", testEnv, err)
+				}
+
+				defer func() {
+					err := os.Unsetenv(testEnv)
+					if err != nil {
+						t.Errorf("Failed to unset %s env variable: %v", testEnv, err)
+					}
+				}()
+			}
+
+			assert.Equal(t, cluster.PostgresImage(), tt.expectedImage)
 		})
 	}
 }
