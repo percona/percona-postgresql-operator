9e7dd15b762012b00630b67f443490b08853d469
Andrii Dema <a.dema@jazzserve.com>
2025-07-22 11:48:28 +0300

    fix merge

--- build/postgres-operator/install-extensions.sh
+++ build/postgres-operator/install-extensions.sh
@@ -16,6 +16,14 @@ if [[ -n $STORAGE_ENDPOINT ]]; then
 	args+=(-endpoint "$STORAGE_ENDPOINT")
 fi
 
+if [[ ${STORAGE_DISABLE_SSL} == "true" ]]; then
+	args+=(-disable-ssl)
+fi
+
+if [[ ${STORAGE_FORCE_PATH_STYLE} == "true" ]]; then
+	args+=(-force-path-style)
+fi
+
 for installed in "${PGDATA_EXTENSIONS}"/*.installed; do
 	filename=$(basename -- "${installed}")
 	key=${filename%.*}
@@ -32,14 +40,6 @@ for installed in "${PGDATA_EXTENSIONS}"/*.installed; do
 		rm -f "${installed}"
 	fi
 done
-
-if [[ ${STORAGE_DISABLE_SSL} == "true" ]]; then
-	args+=(-disable-ssl)
-fi
-
-if [[ ${STORAGE_FORCE_PATH_STYLE} == "true" ]]; then
-	args+=(-force-path-style)
-fi
 
 for key in "${extensions[@]}"; do
 	if [ -f "${PGDATA_EXTENSIONS}"/"${key}".installed ]; then
