ff7182fbab1d1d8c13889b266ef77279cd33c572
Ege Güneş <ege.gunes@percona.com>
2025-09-29 09:32:56 +0300

    K8SPG-844: Don't modify archive_command if trackLatestRestorableTime is disabled

--- internal/pgbackrest/postgres_test.go
+++ internal/pgbackrest/postgres_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"gotest.tools/v3/assert"
+	"k8s.io/utils/ptr"
 
 	"github.com/percona/percona-postgresql-operator/internal/postgres"
 	"github.com/percona/percona-postgresql-operator/pkg/apis/postgres-operator.crunchydata.com/v1beta1"
@@ -20,19 +21,9 @@ func TestPostgreSQLParameters(t *testing.T) {
 
 	PostgreSQL(cluster, parameters, true)
 	assert.DeepEqual(t, parameters.Mandatory.AsMap(), map[string]string{
-		"archive_mode": "on",
-		"archive_command": strings.Join([]string{
-			`pgbackrest --stanza=db archive-push "%p" `,
-			`&& timestamp=$(pg_waldump "%p" | `,
-			`grep -oP "COMMIT \K[^;]+" | `,
-			`sed -E "s/([0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}) (UTC|[\\+\\-][0-9]{2})/\1T\2\3/" | `,
-			`sed "s/UTC/Z/" | `,
-			"tail -n 1 | ",
-			`grep -E "^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}(Z|[\+\-][0-9]{2})$"); `,
-			"if [ ! -z ${timestamp} ]; then echo ${timestamp} > /pgdata/latest_commit_timestamp.txt; fi",
-		}, ""),
-		"restore_command":        `pgbackrest --stanza=db archive-get %f "%p"`,
-		"track_commit_timestamp": "true",
+		"archive_mode":    "on",
+		"archive_command": `pgbackrest --stanza=db archive-push "%p"`,
+		"restore_command": `pgbackrest --stanza=db archive-get %f "%p"`,
 	})
 
 	assert.DeepEqual(t, parameters.Default.AsMap(), map[string]string{
@@ -53,19 +44,9 @@ func TestPostgreSQLParameters(t *testing.T) {
 
 	PostgreSQL(cluster, parameters, true)
 	assert.DeepEqual(t, parameters.Mandatory.AsMap(), map[string]string{
-		"archive_mode": "on",
-		"archive_command": strings.Join([]string{
-			`pgbackrest --stanza=db archive-push "%p" `,
-			`&& timestamp=$(pg_waldump "%p" | `,
-			`grep -oP "COMMIT \K[^;]+" | `,
-			`sed -E "s/([0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}) (UTC|[\\+\\-][0-9]{2})/\1T\2\3/" | `,
-			`sed "s/UTC/Z/" | `,
-			"tail -n 1 | ",
-			`grep -E "^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}(Z|[\+\-][0-9]{2})$"); `,
-			"if [ ! -z ${timestamp} ]; then echo ${timestamp} > /pgdata/latest_commit_timestamp.txt; fi",
-		}, ""),
-		"restore_command":        "/bin/true",
-		"track_commit_timestamp": "true",
+		"archive_mode":    "on",
+		"archive_command": `pgbackrest --stanza=db archive-push "%p"`,
+		"restore_command": "/bin/true",
 	})
 
 	cluster.Spec.Standby = &v1beta1.PostgresStandbySpec{
@@ -73,6 +54,17 @@ func TestPostgreSQLParameters(t *testing.T) {
 		RepoName: "repo99",
 	}
 	cluster.Spec.Patroni.DynamicConfiguration = nil
+
+	PostgreSQL(cluster, parameters, true)
+	assert.DeepEqual(t, parameters.Mandatory.AsMap(), map[string]string{
+		"archive_mode":    "on",
+		"archive_command": `pgbackrest --stanza=db archive-push "%p"`,
+		"restore_command": `pgbackrest --stanza=db archive-get %f "%p" --repo=99`,
+	})
+
+	cluster.Spec.Standby = nil
+	cluster.Spec.Patroni.DynamicConfiguration = nil
+	cluster.Spec.Backups.TrackLatestRestorableTime = ptr.To(true)
 
 	PostgreSQL(cluster, parameters, true)
 	assert.DeepEqual(t, parameters.Mandatory.AsMap(), map[string]string{
@@ -87,7 +79,7 @@ func TestPostgreSQLParameters(t *testing.T) {
 			`grep -E "^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}(Z|[\+\-][0-9]{2})$"); `,
 			"if [ ! -z ${timestamp} ]; then echo ${timestamp} > /pgdata/latest_commit_timestamp.txt; fi",
 		}, ""),
-		"restore_command":        `pgbackrest --stanza=db archive-get %f "%p" --repo=99`,
+		"restore_command":        `pgbackrest --stanza=db archive-get %f "%p"`,
 		"track_commit_timestamp": "true",
 	})
 }
