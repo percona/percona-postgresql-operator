b7c28201d176a417663010a9127df6f37106847d
Mayank Shah <mayankshah1614@gmail.com>
2026-01-16 14:59:26 +0530

    Update percona/controller/pgcluster/standby_test.go
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- percona/controller/pgcluster/standby_test.go
+++ percona/controller/pgcluster/standby_test.go
@@ -15,6 +15,11 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
+
+	"github.com/percona/percona-postgresql-operator/v2/internal/controller/postgrescluster"
+	pNaming "github.com/percona/percona-postgresql-operator/v2/percona/naming"
+	v2 "github.com/percona/percona-postgresql-operator/v2/pkg/apis/pgv2.percona.com/v2"
+	crunchyv1beta1 "github.com/percona/percona-postgresql-operator/v2/pkg/apis/postgres-operator.crunchydata.com/v1beta1"
 )
 
 func TestReconcileStandbyLag(t *testing.T) {
