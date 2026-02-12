b10f5da311804ffc28b2e1dcc683732a9d56885c
Andrii Dema <a.dema@jazzserve.com>
2026-02-12 14:09:51 +0200

    add labels

--- percona/controller/pgcluster/pvc.go
+++ percona/controller/pgcluster/pvc.go
@@ -8,6 +8,7 @@ import (
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	"github.com/percona/percona-postgresql-operator/v2/internal/naming"
 	v2 "github.com/percona/percona-postgresql-operator/v2/pkg/apis/pgv2.percona.com/v2"
 	"github.com/percona/percona-postgresql-operator/v2/pkg/apis/postgres-operator.crunchydata.com/v1beta1"
 )
@@ -17,17 +18,43 @@ func (r *PGClusterReconciler) reconcilePVCs(ctx context.Context, cr *v2.PerconaP
 		return nil
 	}
 	for _, set := range cr.Spec.InstanceSets {
-		if err := ensureSidecarPVCs(ctx, r.Client, cr.Namespace, set.SidecarPVCs); err != nil {
+		ls := naming.Merge(
+			cr.Spec.Metadata.GetLabelsOrNil(),
+			set.Metadata.GetLabelsOrNil(),
+			naming.WithPerconaLabels(
+				map[string]string{
+					naming.LabelCluster:     cr.Name,
+					naming.LabelInstanceSet: set.Name,
+					naming.LabelData:        naming.DataPostgres,
+				},
+				cr.GetName(), "pg", cr.Spec.CRVersion),
+		)
+		if err := ensureSidecarPVCs(ctx, r.Client, cr, set.SidecarPVCs, ls); err != nil {
 			return errors.Wrap(err, "failed to create instance set pvcs")
 		}
 	}
 	if cr.Spec.Backups.PGBackRest.RepoHost != nil && len(cr.Spec.Backups.PGBackRest.RepoHost.SidecarPVCs) > 0 {
-		if err := ensureSidecarPVCs(ctx, r.Client, cr.Namespace, cr.Spec.Backups.PGBackRest.RepoHost.SidecarPVCs); err != nil {
+		ls := naming.Merge(
+			cr.Spec.Metadata.GetLabelsOrNil(),
+			cr.Spec.Backups.PGBackRest.Metadata.GetLabelsOrNil(),
+			naming.WithPerconaLabels(
+				naming.PGBackRestLabels(cr.GetName()),
+				cr.GetName(), "", cr.Spec.CRVersion),
+		)
+		if err := ensureSidecarPVCs(ctx, r.Client, cr, cr.Spec.Backups.PGBackRest.RepoHost.SidecarPVCs, ls); err != nil {
 			return errors.Wrap(err, "failed to create repo host sidecar pvcs")
 		}
 	}
 	if cr.Spec.Proxy != nil && cr.Spec.Proxy.PGBouncer != nil && len(cr.Spec.Proxy.PGBouncer.SidecarPVCs) > 0 {
-		if err := ensureSidecarPVCs(ctx, r.Client, cr.Namespace, cr.Spec.Proxy.PGBouncer.SidecarPVCs); err != nil {
+		ls := naming.Merge(
+			cr.Spec.Metadata.GetLabelsOrNil(),
+			cr.Spec.Proxy.PGBouncer.Metadata.GetLabelsOrNil(),
+			naming.WithPerconaLabels(map[string]string{
+				naming.LabelCluster: cr.Name,
+				naming.LabelRole:    naming.RolePGBouncer,
+			}, cr.Name, "pgbouncer", cr.Spec.CRVersion),
+		)
+		if err := ensureSidecarPVCs(ctx, r.Client, cr, cr.Spec.Proxy.PGBouncer.SidecarPVCs, ls); err != nil {
 			return errors.Wrap(err, "failed to create pgbouncer sidecar pvcs")
 		}
 	}
@@ -38,14 +65,16 @@ func (r *PGClusterReconciler) reconcilePVCs(ctx context.Context, cr *v2.PerconaP
 func ensureSidecarPVCs(
 	ctx context.Context,
 	cl client.Client,
-	namespace string,
+	cr *v2.PerconaPGCluster,
 	pvcs []v1beta1.SidecarPVC,
+	ls map[string]string,
 ) error {
 	for _, sidecarPVC := range pvcs {
 		pvc := new(corev1.PersistentVolumeClaim)
 		pvc.Name = sidecarPVC.Name
-		pvc.Namespace = namespace
+		pvc.Namespace = cr.Namespace
 		pvc.Spec = sidecarPVC.Spec
+		pvc.Labels = ls
 
 		err := cl.Get(ctx, client.ObjectKeyFromObject(pvc), &corev1.PersistentVolumeClaim{})
 		if err == nil {
