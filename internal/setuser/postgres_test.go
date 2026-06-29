2b4fe57a824c172f355859d7d24f81666f8de682
Andrii Dema <a.dema@jazzserve.com>
2026-06-29 13:06:26 +0300

    K8SPG-1040: add `pg_cron` and `set_user` builtin extensions
    https://perconadev.atlassian.net/browse/K8SPG-1040

---
+++ internal/setuser/postgres_test.go
@@ -0,0 +1,84 @@
+package setuser
+
+import (
+	"context"
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/pkg/errors"
+	"gotest.tools/v3/assert"
+
+	"github.com/percona/percona-postgresql-operator/v2/internal/postgres"
+)
+
+func TestEnableInPostgreSQL(t *testing.T) {
+	expected := errors.New("whoops")
+	exec := func(
+		_ context.Context, stdin io.Reader, stdout, stderr io.Writer, command ...string,
+	) error {
+		assert.Assert(t, stdout != nil, "should capture stdout")
+		assert.Assert(t, stderr != nil, "should capture stderr")
+
+		assert.Assert(t, strings.Contains(strings.Join(command, "\n"),
+			`SELECT datname FROM pg_catalog.pg_database`,
+		), "expected all databases and templates")
+
+		b, err := io.ReadAll(stdin)
+		assert.NilError(t, err)
+		assert.Equal(t, string(b), strings.Trim(`
+SET client_min_messages = WARNING; CREATE EXTENSION IF NOT EXISTS set_user; ALTER EXTENSION set_user UPDATE;
+		`, "\t\n"))
+
+		return expected
+	}
+
+	ctx := context.Background()
+	assert.Equal(t, expected, EnableInPostgreSQL(ctx, exec))
+}
+
+func TestDisableInPostgreSQL(t *testing.T) {
+	expected := errors.New("whoops")
+	exec := func(
+		_ context.Context, stdin io.Reader, stdout, stderr io.Writer, command ...string,
+	) error {
+		assert.Assert(t, stdout != nil, "should capture stdout")
+		assert.Assert(t, stderr != nil, "should capture stderr")
+
+		assert.Assert(t, strings.Contains(strings.Join(command, "\n"),
+			`SELECT datname FROM pg_catalog.pg_database`,
+		), "expected all databases and templates")
+
+		b, err := io.ReadAll(stdin)
+		assert.NilError(t, err)
+		assert.Equal(t, string(b), strings.Trim(`
+SET client_min_messages = WARNING; DROP EXTENSION IF EXISTS set_user;
+		`, "\t\n"))
+
+		return expected
+	}
+
+	ctx := context.Background()
+	assert.Equal(t, expected, DisableInPostgreSQL(ctx, exec))
+}
+
+func TestPostgreSQLParameters(t *testing.T) {
+	parameters := postgres.Parameters{
+		Mandatory: postgres.NewParameterSet(),
+	}
+
+	PostgreSQLParameters(&parameters)
+
+	assert.Assert(t, parameters.Default == nil)
+	assert.DeepEqual(t, parameters.Mandatory.AsMap(), map[string]string{
+		"shared_preload_libraries": "set_user",
+	})
+
+	parameters.Mandatory.Add("shared_preload_libraries", "some,existing")
+	PostgreSQLParameters(&parameters)
+
+	assert.Assert(t, parameters.Default == nil)
+	assert.DeepEqual(t, parameters.Mandatory.AsMap(), map[string]string{
+		"shared_preload_libraries": "some,existing,set_user",
+	})
+}
